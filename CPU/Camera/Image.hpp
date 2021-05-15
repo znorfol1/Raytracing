9017b2d7d26e6836d7349f04ca8bf0a7b6c9372c
znorfol1 <78775559+znorfol1@users.noreply.github.com>
2021-05-15 10:09:46 -0400

    Improved documentation

--- CPU/Camera/Image.hpp
+++ CPU/Camera/Image.hpp
@@ -10,68 +10,20 @@
 
 #include <stdio.h>
 #include <string>
+#include "Color.hpp"
 
 /*
- Defines the RGB struct and Image class. The RGB struct represents an RGB color value. The Color
- namespace contains common color values as RGB objects. 
- */
-
-struct RGB{
-    int r;
-    int g;
-    int b;
-    
-    RGB(int r, int g, int b): r(r), g(g), b(b) {};
-    RGB():r(0), g(0), b(0) {};
-    
-    RGB scale(double x) const{
-        if (x < 0){
-            x = 0;
-        } else if(x > 1){
-            x = 1;
-        }
-        double a= 255/2;
-        if(r==125 &&b==125  && g==125){
-            return RGB(scaleFunc2(r, x), scaleFunc2(g, x), scaleFunc2(b, x));
-        }
-        return RGB(scaleFunc(r, x), scaleFunc(g, x), scaleFunc(b, x));
-    }
-private:
-    static int scaleFunc(double orig, double x){
-        double a = 4*255;
-        double b = -4*orig-a;
-        double c = -.75*a-b;
-        double y = a*x*x*x + b*x*x + c*x;
-        return y/2+20;
-        
-    }
-                       
-                       static int scaleFunc2(double orig, double x){
-                           double a = 4*255;
-                           double b = -4*orig-a;
-                           double c = -.75*a-b;
-                           double y = a*x*x*x + b*x*x + c*x;
-                           return y/3+40;
-                           
-                       }
-};
-
-namespace Color{
-    const static RGB WHITE(255, 255, 255);
-    const static RGB BLACK(3, 3, 3);
-    const static RGB VOID(36, 36, 36);
-    const static RGB ORANGE(255, 188, 5);
-    const static RGB GRAY(125, 125, 125);
-const static RGB BLUE(24, 61, 182);
-}
-
-
-
+ An Image represents a PPM image file loaded in memory. It can be written to an output location using
+ the function writeTo().
+*/
 class Image{
+    
     unsigned int x;
     unsigned int y;
     RGB* pixels;
+    
 public:
+ 
     Image(int x=1920, int y=1080): x(x), y(y) {
         pixels = new RGB[x*y];
     }
@@ -84,13 +36,13 @@ public:
         return pixels+x*i;
     }
    
-    void writeTo(std::string filename, int max){
+    void writeTo(std::string filename){
         FILE *f = fopen(filename.data(), "w");
-        fprintf(f, "P3 %d %d %d\n", x, y, max);
+        fprintf(f, "P3 %d %d %d\n", x, y, 255);
         for(int i = 0; i < y; i++){
             for(int j = 0; j < x; j++){
                 RGB p = pixels[i*x + j];
-                fprintf(f,"%d %d %d ", p.r, p.g, p.b);
+                fprintf(f,"%d %d %d\n", p.r, p.g, p.b);
             }
             fputc('\n', f);
         }
