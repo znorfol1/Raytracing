62406a653133147c9f5f51f0d8be7d9e803dea3d
znorfol1 <78775559+znorfol1@users.noreply.github.com>
2021-05-16 15:18:06 -0400

    Added bird

--- CPU/Scene/Solids/Solid.hpp
+++ CPU/Scene/Solids/Solid.hpp
@@ -9,31 +9,50 @@
 #define Solid_hpp
 
 #include "Ray.hpp"
-#include "Point.hpp"
-#include "Image.hpp"
+#include "Color.hpp"
+#include <cmath>
+
+#define SOLID_EPSILON 0.000001
 
 /*
  Defines the abstract Solid class. Solids are the objects in the scene which interact with
- light. These are the material objects to be rendered.
+ light. These are the objects that show up in the rendering. Solids have a color and reflectivity value.
+ Reflectivity is currently unsupported, all objects are diffuse.
  */
-
 class Solid{
 public:
     RGB color;
-    Solid(RGB c = Color::VOID) : color(c) {};
+    double reflectivity; //Must be in [0,1]
+    //  TODO: Add support for reflectivity
+    
+    Solid(RGB c = Color::WHITE, double r = 0) : color(c), reflectivity(r){};
+    
+    Solid(const Solid &s):  Solid(s.color, s.reflectivity) {};
     
     /*
      Determimes the intersection Point of a Ray with the Solid. If the Ray does not
      intersect, POINT_NAN is returned.
      */
-    virtual Point intersects(Ray r) const = 0;
+    virtual Point intersect(const Ray &r) const = 0;
+    
+    /*
+     Returns the vector normal to the surface at Point p on the surface.
+     */
+    virtual UnitVec normal(const Point &p) const = 0;
     
     /*
      Finds the reflection of the given Ray off of the Solid. The origin of the resulting Ray
      is the Point of intersection.
      */
-    virtual Ray reflect(Ray r) const = 0;
+    virtual Ray reflect(const Ray &r) const = 0;
+    
+    /*
+     Returns the amount of light from an incoming Ray that gets reflected in the outgoing
+     direction. Currenty it is constant as only diffuse objects are supported.
+     */
+    double brdf(const Ray &in, const UnitVec &out) const{
+        return 1;
+    }
 };
-
 
 #endif /* Solid_hpp */
