9461634303c0efe874e2c8709aea339fad94929d
znorfol1 <78775559+znorfol1@users.noreply.github.com>
2021-05-16 15:19:06 -0400

    Improved  documentation

--- CPU/Scene/Solids/Sphere.hpp
+++ CPU/Scene/Solids/Sphere.hpp
@@ -11,41 +11,44 @@
 #include "Solid.hpp"
 #include "Plane.hpp"
 #include <cmath>
-#include <iostream>
 
 #define SQR(x) ((x)*(x))
 
 /*
- Defines the Sphere class. A Sphere is defined by its center Point and a radius.
+ Defines the Sphere class. A Sphere is determined by its center Point and a radius value.
 */
-
-
 class Sphere: public Solid{
-    Point c;
-    double radius;
-    
-    constexpr const static double EPSILON = .000001;
+    Point c; // center of Sphere
+    double radius;  // radius
 public:
-    Sphere(Point c, double r, RGB color): Solid(color), c(c), radius(r) {};
+    Sphere(Point c, double r, RGB color=Color::WHITE, double refl=0): Solid(color, refl), c(c), radius(r) {};
     
-    virtual Point intersects(Ray r) const override{
+    virtual Point intersect(const Ray &r) const override{
         Point u = r.direction;
         Point o = r.origin;
         double del = SQR(u.dot(o-c)) + SQR(radius) - SQR((o-c).norm());
-        if(del ==0){
+        if(del < 0){
             return POINT_NAN;
         }
         double d = -(u.dot(o-c)) - sqrt(del);
-        if(d <=0-EPSILON){
+        if(d <= 0-SOLID_EPSILON){
             return POINT_NAN;
         }
         return o + d*u;
     }
     
-    virtual Ray reflect(Ray r) const override{
-        Point intersection = intersects(r);
+    virtual UnitVec normal(const Point &p) const override{
+        return Ray(c, p).direction;
+    }
+    
+    virtual Ray reflect(const Ray &r) const override{
+        Point intersection = intersect(r);
         Ray pos(intersection, (2*intersection)-c);
         Plane plane(pos);
+        Ray a = plane.reflect(r);
+        if(Point::isNan(a.origin)){
+            return Ray(r.origin+SOLID_EPSILON*r.direction, r.origin+r.direction);
+        }
         return plane.reflect(r);
     }
 };
