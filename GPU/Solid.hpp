38208bff763ada077314c014e01a78b2770c6d6e
znorfol1 <78775559+znorfol1@users.noreply.github.com>
2021-05-17 13:56:34 -0400

    Update Solid.hpp

--- GPU/Solid.hpp
+++ GPU/Solid.hpp
@@ -9,26 +9,23 @@
 #define Solid_hpp
 
 #include "Ray.hpp"
-#include "Point.hpp"
 #include "Color.hpp"
-#include <cmath>
 
-#define SOLID_EPSILON 0.00001
+#define SOLID_EPSILON 0.000001
 
 /*
  Defines the abstract Solid class. Solids are the objects in the scene which interact with
- light. These are the material objects to be rendered.
+ light. These are the objects that show up in the rendering. Solids have a color and reflectivity value.
+ Reflectivity is currently unsupported, so all objects are diffuse.
  */
-
 class Solid{
-protected:
-    constexpr const static double EPSILON = 0.000001;
 public:
     RGB color;
     double reflectivity; //Must be in [0,1]
+    //  TODO: Add support for reflectivity
     
     __device__
-    Solid(RGB c , double r = .999) : color(c), reflectivity(r){};
+    Solid(RGB c = WHITE, double r = 0) : color(c), reflectivity(r){};
     
     __device__
     Solid(const Solid &s):  Solid(s.color, s.reflectivity) {};
@@ -41,41 +38,31 @@ public:
      intersect, POINT_NAN is returned.
      */
     __device__
-    virtual Point intersects(Ray r) const = 0;
+    virtual Point intersect(const Ray &r) const = 0;
     
     /*
-     Returns the vector normal to the surface at Point p.
+     Returns the vector normal to the surface at Point p on the surface.
      */
     __device__
-    virtual UnitVec normal(Point p) const = 0;
+    virtual UnitVec normal(const Point &p) const = 0;
     
     /*
      Finds the reflection of the given Ray off of the Solid. The origin of the resulting Ray
      is the Point of intersection.
      */
     __device__
-    virtual Ray reflect(Ray r) const = 0;
+    virtual Ray reflect(const Ray &r) const = 0;
     
     /*
      Returns the amount of light from an incoming Ray that gets reflected in the outgoing
-     direction.
+     direction. Currenty it is constant as only diffuse objects are supported.
      */
     __device__
-    double brdf(Ray in, UnitVec out){
-        Ray refl = reflect(in);
-        double x = out.dot(refl.direction);
-        if(x>1){
-            x = 1;
-        }
-        if(x < 0){
-            x = 0;
-        }
-        x = (x+1)/2;
-
-        return -5*log(1-reflectivity) * (1-x)+.0001;
+    double brdf(const Ray &in, const UnitVec &out) const{
+        return 1;
     }
 };
 
-
 #endif /* Solid_hpp */
+
 
