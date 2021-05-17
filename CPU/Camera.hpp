82333d35773d6c7f62353a60daf7598b3051779a
znorfol1 <78775559+znorfol1@users.noreply.github.com>
2021-05-17 12:20:46 -0400

    Update Camera.hpp

--- CPU/Camera.hpp
+++ CPU/Camera.hpp
@@ -8,9 +8,9 @@
 #ifndef Camera_hpp
 #define Camera_hpp
 
-#include "Ray.hpp"
 #include "Scene.hpp"
 #include "Image.hpp"
+#include <chrono>
 #include <iostream>
 
 /*
@@ -20,17 +20,16 @@
  ratio with y being 1 unit in lenth relative to the coordinate system used by the scene.
  */
 class Camera{
-    
     double focalLength;
     double viewPortX;
     double viewPortY;
-    
 public:
     
     Camera(double f = 1, double x=1.77777777, double y=1): viewPortX(x), viewPortY(y), focalLength(f) {};
 
     /*
-        Uses this camera to produce an image of the scene.
+        Uses this camera to produce an image of the scene. The resolution
+        should conform to the aspect ratio of the viewport
      
         xRes: x-resolution of the output image
         yRes: y-resolution of the output image
@@ -45,18 +44,26 @@ public:
         Point yBasis = UnitVec(position.direction.cross(xBasis))*(-viewPortY/yRes);
         Point o = position.origin + position.direction*focalLength - (xRes/2)*xBasis + (yRes/2)*yBasis;
         // o is the location of the top left of the outputted image
+        
         double last = 0;
+        double timeMS = 0;
+        
         for(int i = 0; i < xRes; i++){
+            auto startTime = std::chrono::system_clock::now();
             for (int j = 0; j < yRes; j++){
                 Ray p(position.origin, o + i*xBasis - j*yBasis);
                 img[j][i] = scene.trace(p);
             }
-            double progress = (((double)i)/xRes);
+            auto endTime = std::chrono::system_clock::now();
+            
+            timeMS += std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime).count();
+            double progress = ((double)i)/xRes;
             if(progress > last){
                 printf("%.0lf percent complete\n", progress*100);
                 last += .1;
             }
         }
+        printf("Total time taken: %.2lf seconds\n\n", timeMS/1000);
         return img;
     }
     
