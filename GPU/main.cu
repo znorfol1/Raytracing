a63b7ae4047799ca9460b1c73c5c4e55283d0d1a
znorfol1 <78775559+znorfol1@users.noreply.github.com>
2021-05-18 00:44:14 -0400

    Update main.cu

--- GPU/main.cu
+++ GPU/main.cu
@@ -1,4 +1,3 @@
-
 #include "Scene.hpp"
 #include "Ray.hpp"
 #include "Plane.hpp"
@@ -8,27 +7,23 @@
 #include <iostream>
 #include "Sphere.hpp"
 #include "Camera.hpp"
-       
+    
     
 int main() {
-    createSceneGPU(2);
+    //Set stack
+    cudaDeviceSetLimit(cudaLimitStackSize, 32768ULL);
+
+    createSceneGPU(3);
     
     Ray r1(Point(0,0,0), Point(0,1,0));
-    createPlaneGPU(r1, RGB(40,40,40));
-    gpuErrchk( cudaPeekAtLastError() );
-    gpuErrchk( cudaDeviceSynchronize() );
- 
-    createSphereGPU(Point(0,1,0), 1, RGB(255, 188, 5));
-    gpuErrchk( cudaPeekAtLastError() );
-    gpuErrchk( cudaDeviceSynchronize() );
-    
-    createLightGPU(Point(0,6,0), WHITE);
-    gpuErrchk( cudaPeekAtLastError() );
-    gpuErrchk( cudaDeviceSynchronize() );
-    
-    
+    createPlaneGPU(r1, RGB(255,255,255));
+    createSphereGPU(Point(0,1,0),1, Color::YELLOW);
+    createSphereGPU(Point(-4,1,1), 1, Color::GREEN);
+    createLightGPU(Point(0,4,1), WHITE);
+  
     Camera cam;
-    cam.render(Ray(Point(3,5,3), Point(0,1,0))).writeTo("out.ppm");
-    gpuErrchk( cudaPeekAtLastError() );
-    gpuErrchk( cudaDeviceSynchronize() );
+    Ray camPosition(Point(4,3,4), Point(0,1,0));
+    cam.render(camPosition).writeTo("out.ppm");
+       
+    return  0;
 }
